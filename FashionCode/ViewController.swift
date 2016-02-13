cd465f679c2e5f021144c7de49b45bd341e8d4da
SEKINE YURI <yuri.1027ex@gmail.com>
2016-02-14 03:38:50 +0900

    雰囲気

--- FashionCode/ViewController.swift
+++ FashionCode/ViewController.swift
@@ -7,13 +7,58 @@
 //
 
 import UIKit
+import Social
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func precentPickerController(sourceType: UIImagePickerControllerSourceType) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(sourceType) {
+            
+            let picker = UIImagePickerController()
+            picker.sourceType = sourceType
+            picker.delegate = self
+            self.presentViewController(picker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: NSDictionary!) {
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+//        photoImageView.image = image
+    }
+    
+    @IBAction func registerButtonTapped(sender: UIButton) {
+     
+        let alertController = UIAlertController(title: "画像の取得先を選択", message: nil, preferredStyle: .ActionSheet)
+        
+        let firstAction = UIAlertAction(title: "カメラ", style: .Default) {
+            action in
+            self.precentPickerController(.Camera)
+        }
+        
+        let secondAction = UIAlertAction(title: "アルバム", style: .Default) {
+            action in
+            self.precentPickerController(.PhotoLibrary)
+        }
+        let cancelAction = UIAlertAction(title: "キャンセル", style:  .Cancel, handler: nil)
+        
+        alertController.addAction(firstAction)
+        alertController.addAction(secondAction)
+        alertController.addAction(cancelAction)
+        
+        presentViewController(alertController, animated: true, completion: nil)
+    
+    }
+    
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
